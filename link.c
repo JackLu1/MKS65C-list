3d75377d2d291d532e0d31592258f83a8f74f2ed
Jack Lu <jlu9@stuy.edu>
2018-10-09 15:34:39 -0400

    working

---
+++ link.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "link.h"
+
+void print_list(struct node * n){
+    printf("node\n");
+}
+
+struct node * insert_front(struct node * n, int i){
+    return n;
+}
+
+struct node * free_list(struct node * n){
+    return n;
+}
